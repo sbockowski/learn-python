388033215ee0c7854ae0b7a676acd9c43220e8c4
Szymon Boćkowski <sbockowx@gmail.com>
2026-01-21 13:46:16 +0100

    Implemented the Add button

--- APP_1-TODO/functions.py
+++ APP_1-TODO/functions.py
@@ -7,7 +7,7 @@ def get_todos(filepath=FILEPATH):
     to do items.
     """
     with open(filepath,'r') as file_local:
-        todos_local = file_local.readline()
+        todos_local = file_local.readlines()
     return todos_local
 
 def write_todos(todos_arg, filepath=FILEPATH):
--- APP_1-TODO/gui.py
+++ APP_1-TODO/gui.py
@@ -2,11 +2,24 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText()
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 complete_button = sg.Button("Complete")
 
-window = sg.Window("My To-Do App", layout=[[label], [input_box, add_button]])
+window = sg.Window("My To-Do App", 
+                    layout=[[label], [input_box, add_button]],
+                    font=('Helvetica', 14))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos =  functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
 
-window.read()
 window.close()
