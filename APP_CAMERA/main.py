450031487c79c182fd8e0e58259a26a153cdbd38
Szymon Boćkowski <sbockowx@gmail.com>
2026-01-23 14:54:25 +0100

    Implement upload image function

--- APP_CAMERA/main.py
+++ APP_CAMERA/main.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from PIL import Image
 
+st.subheader("Color to Grayscale Converter")
+
+uploaded_image = st.file_uploader("Upload Image")
+
 with st.expander("Start Camera"):
     # Start the camera
     camera_image = st.camera_input("Camera")
@@ -8,6 +12,15 @@ with st.expander("Start Camera"):
 if camera_image:
     # Create a pilow image instance
     img = Image.open(camera_image)
+
+    # Convert the pillow image to grayscale
+    gray_img = img.convert("L")
+    # Render the grayscale image on the webpage
+    st.image(gray_img)
+
+if uploaded_image:
+    # Create a pilow image instance
+    img = Image.open(uploaded_image)
 
     # Convert the pillow image to grayscale
     gray_img = img.convert("L")
