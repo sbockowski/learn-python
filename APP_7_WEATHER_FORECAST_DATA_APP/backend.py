5a5c45ee51de644e934841974e0396f252308df5
Szymon Boćkowski <sbockowx@gmail.com>
2026-01-28 22:55:33 +0100

    Show sky information with images and fix data scale

--- APP_7_WEATHER_FORECAST_DATA_APP/backend.py
+++ APP_7_WEATHER_FORECAST_DATA_APP/backend.py
@@ -8,6 +8,7 @@ def get_data(place, forecast_days=None):
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
+    
     return filtered_data
 
 if __name__ == "__main__":
