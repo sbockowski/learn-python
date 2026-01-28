b0f028ba3cd8fac62e9033a69d25702a6c2ec234
Szymon Boćkowski <sbockowx@gmail.com>
2026-01-28 10:07:08 +0100

    Finished app

--- APP_6_WEATHER_API_APP/main.py
+++ APP_6_WEATHER_API_APP/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import pandas as pd
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
@@ -12,6 +12,7 @@ def temperature(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
+
     return {
         "station": station,
         "date": date,
--- APP_WORD_DEFINITION_API/main.py
+++ APP_WORD_DEFINITION_API/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask("__name__")
 
+df = pd.read_csv("dictionary.csv")
 
 @app.route("/")
 def home():
@@ -9,9 +11,9 @@ def home():
 
 @app.route("/api/v1/<word>")
 def api(word):
-    return {"definition": word.upper(),
-            "word": word,
-            }
+    definition = df.loc[df["word"] == word]["definition"].squeeze()
+    result_dictionary = {'word': word, 'definition': definition}
+    return result_dictionary
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
